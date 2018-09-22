ddce9e8d5a24f8542fb75e01f9560994d3664101
Ryota <andoooo9.y.1127@gmail.com>
2018-09-22 17:57:45 +0900

    取得したAPI情報をJsonファイルに変換する処理を追加

---
+++ Assets/Scripts/ConvertToJson.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToJson : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CreateJson(string fetch){
+        ResultSearchRepository rsp = new ResultSearchRepository();
+        rsp = JsonUtility.FromJson<ResultSearchRepository>(fetch);
+        Debug.Log("total_count: " + rsp.total_count);
+    }
+}
--- Assets/Scripts/FetchRepositoryData.cs
+++ Assets/Scripts/FetchRepositoryData.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using MiniJSON;
 
-public class FetchRepositoryData : MonoBehaviour {
+public class FetchRepositoryData : MonoBehaviour
+{
 
     private string url = "https://api.github.com/search/repositories?q=React+in:name&sort=stars";
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         print("start関数");
         StartCoroutine("StartConnection");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public IEnumerator StartConnection()
     {
@@ -33,7 +37,38 @@ public class FetchRepositoryData : MonoBehaviour {
             yield return www;
 
             print(www.text);
-        }
+            string fetchJson = www.text;
 
+            // -----------------------------
+            // JSONをDictionaryに変換する処理
+            // -----------------------------
+            //Dictionary<string, object> fetchApiDic = Json.Deserialize(fetchJson) as Dictionary<string, object>;
+
+            // JSONデータは最初は配列から始まるので、Deserialize（デコード）した直後にリストへキャスト      
+            IList fetchRepoList = (IList)Json.Deserialize(fetchJson);
+            
+            List<Dictionary<string, string>> itemDicList = new List<Dictionary<string, string>>();
+
+            foreach(IDictionary fetch in fetchRepoList){
+                string total_count = (string)fetch["total_count"];
+                IList items = (IList)fetch["items"];
+
+                foreach (IDictionary item in items)
+                {
+                    Dictionary<string, string> itemDic = new Dictionary<string, string>();
+                    itemDic.Add("name", (string)item["name"]);
+                    itemDic.Add("full_name", (string)item["full_name"]);
+                    itemDic.Add("url", (string)item["url"]);
+
+                    // リストに追加
+                    itemDicList.Add(itemDic);
+                }
+            }
+
+            print("name: " + itemDicList[0]["name"]);
+            print("full_name: " + itemDicList[0]["full_name"]);
+            print("url: " + itemDicList[0]["url"]);
+
+        }
     }
 }
