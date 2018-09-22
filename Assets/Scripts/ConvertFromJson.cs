9df1d5df6ada80ebc10dfd28de478dc554d47623
Ryota <andoooo9.y.1127@gmail.com>
2018-09-23 02:05:07 +0900

    APIを叩いて取得したデータから必要なパラメータを抽出する処理を追加

--- Assets/Scripts/ConvertFromJson.cs
+++ Assets/Scripts/ConvertFromJson.cs
@@ -7,7 +7,7 @@ public class ConvertFromJson : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        ConvertToDictionary();
 	}
 	
 	// Update is called once per frame
@@ -26,12 +26,22 @@ public class ConvertFromJson : MonoBehaviour {
                                             "\"name\": \"react\"," +
                                             "\"full_name\": \"facebook/react\"," +
                                             "\"html_url\": \"https://github.com/facebook/react\"" +
+                                            "\"owner\": [" +
+                                                "{" +
+                                                    "\"avatar_url\": \"https://avatars3.githubusercontent.com/u/69631?v=4\"" +
+                                                "}" +
+                                            "]" +
                                         "}," +
                                         "{" +
                                             "\"id\": 29028775," +
                                             "\"name\": \"react-native\"," +
                                             "\"full_name\": \"facebook/react-native\"," +
                                             "\"html_url\": \"https://github.com/facebook/react-native\"" +
+                                            "\"owner\": [" +
+                                                "{" +
+                                                    "\"avatar_url\": \"https://avatars2.githubusercontent.com/u/1696866?v=4\"" +
+                                                "}" +
+                                            "]" +
                                         "}" +
                                     "]" +
                                 "}" +
@@ -55,12 +65,20 @@ public class ConvertFromJson : MonoBehaviour {
                 itemDic.Add("full_name", item["full_name"].ToString());
                 itemDic.Add("html_url", item["html_url"].ToString());
 
+
+                IList owners = (IList)item["owner"];
+                foreach (IDictionary owner in owners)
+                {
+                    itemDic.Add("avatar_url", owner["avatar_url"].ToString());
+                }
+
                 // リストに追加
                 itemDicList.Add(itemDic);
             }
         }
 
         print("name: " + itemDicList[0]["name"]);
+        print("avatar_url: " + itemDicList[0]["avatar_url"]);
         print("full_name: " + itemDicList[0]["full_name"]);
         print("html_url: " + itemDicList[0]["html_url"]);
     }
