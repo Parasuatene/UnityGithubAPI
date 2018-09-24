1e3389ef368ebb36f0dafbf7e2dccb8bc7b75a60
Ryota <andoooo9.y.1127@gmail.com>
2018-09-24 23:17:05 +0900

    削除してしまったデータを復元

--- Assets/Scripts/SearchButton.cs
+++ Assets/Scripts/SearchButton.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SearchButton : MonoBehaviour {
+public class SearchPanel : MonoBehaviour
+{
 
     [SerializeField] GameObject inputFieldObj;
     private InputField inputField;
@@ -11,8 +12,9 @@ public class SearchButton : MonoBehaviour {
     [SerializeField] GameObject searchRepoObj;
     private SearchRepository searchRepo;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         inputField = inputFieldObj.GetComponent<InputField>();
         button = gameObject.GetComponent<Button>();
         searchRepo = searchRepoObj.GetComponent<SearchRepository>();
@@ -21,32 +23,23 @@ public class SearchButton : MonoBehaviour {
     void Update()
     {
         // 何も入力されていない時はSearch出来ないようにする
-        if(inputField.text == ""){
+        if (inputField.text == "")
+        {
             button.interactable = false;
-        }else{
+        }
+        else
+        {
             button.interactable = true;
         }
     }
 
     // リポジトリ検索ボタン
-    public void SearchRepository(){
+    public void SearchRepository()
+    {
         string repoName = inputField.text;
         print(repoName);
-        string queryParam = CreateQueryParam(repoName);
-
+        string queryParam = GameObject.Find("QueryParamManager").GetComponent<QueryParamManager>().CreateQueryParam(repoName);
         // 通信開始
         StartCoroutine(searchRepo.StartConnection(queryParam));
-    }
-
-    // 与えられたパラメータからクエリを作成して返却
-    private string CreateQueryParam(string repoName, int pageNumber = 1, string sort = "", string order = "")
-    {
-        string pageParam = "page=" + pageNumber;
-        string repoParam = "&q=" + repoName;
-        // sortとorderは指定がある場合, 先頭に&を付与する
-        string sortParam = (sort == "" ? "" : "&" + sort);
-        string orderParam = (order == "" ? "" : "&" + order);
-
-        return pageParam + repoParam + sortParam + orderParam;
     }
 }
