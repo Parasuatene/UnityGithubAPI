cad60704ce7ae0ae041ab25ad3da106f9c667368
Ryota <andoooo9.y.1127@gmail.com>
2018-09-24 23:04:08 +0900

    Unityに不具合が生じたので一度コミット

--- Assets/Scripts/SearchPanel.cs
+++ Assets/Scripts/SearchPanel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SearchButton : MonoBehaviour {
+public class SearchPanel : MonoBehaviour {
 
     [SerializeField] GameObject inputFieldObj;
     private InputField inputField;
@@ -32,21 +32,8 @@ public class SearchButton : MonoBehaviour {
     public void SearchRepository(){
         string repoName = inputField.text;
         print(repoName);
-        string queryParam = CreateQueryParam(repoName);
-
+        string queryParam = GameObject.Find("QueryParamManager").GetComponent<QueryParamManager>().CreateQueryParam(repoName);
         // 通信開始
         StartCoroutine(searchRepo.StartConnection(queryParam));
-    }
-
-    // 与えられたパラメータからクエリを作成して返却
-    private string CreateQueryParam(string repoName, int pageNumber = 1, string sort = "", string order = "")
-    {
-        string pageParam = "page=" + pageNumber;
-        string repoParam = "&q=" + repoName;
-        // sortとorderは指定がある場合, 先頭に&を付与する
-        string sortParam = (sort == "" ? "" : "&" + sort);
-        string orderParam = (order == "" ? "" : "&" + order);
-
-        return pageParam + repoParam + sortParam + orderParam;
     }
 }
