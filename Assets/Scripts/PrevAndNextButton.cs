e31c740ed9fa18a56ee8775e91ebf9522d276783
Ryota <andoooo9.y.1127@gmail.com>
2018-09-24 23:20:28 +0900

    PrevとNextボタン用のScriptを作成

---
+++ Assets/Scripts/PrevAndNextButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ButtonType
+{
+    prev, next
+};
+
+public class PrevAndNextButton : MonoBehaviour
+{
+
+    private QueryParamManager queryParamManager;
+    private int pageNumber;
+
+    // Use this for initialization
+    void Start()
+    {
+        queryParamManager = GameObject.Find("QueryParamManager").GetComponent<QueryParamManager>();
+        pageNumber = queryParamManager.GetPageNumber();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
